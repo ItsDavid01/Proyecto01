50a72ceaa07d3900d2a5bbbd156a461be6b32be1
ItsDavid <mezaad@uninorte.edu.co>
2025-05-11 21:18:48 -0500

    changed some json message responses for everything to be in spanihs

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,11 +4,11 @@ const User = require('../models/user.model');
 
 exports.authenticate = async (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
-  if (!token) return res.status(401).json({ message: 'No token' });
+  if (!token) return res.status(401).json({ message: 'sin token' });
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(payload.sub);
-    if (!user || !user.isActive) return res.status(401).json({ message: 'Invalid token' });
+    if (!user || !user.isActive) return res.status(401).json({ message: 'token invalido' });
     req.user = user;
     next();
   } catch {
